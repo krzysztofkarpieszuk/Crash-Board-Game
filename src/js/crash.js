c779689260919359268a0f3163caddd0549f590e
Krzysztof Karpieszuk <krzysztof.karpieszuk@gmail.com>
2018-11-19 14:59:01 +0100

    Crash class created

---
+++ src/js/crash.js
@@ -0,0 +1,7 @@
+export default class Crash {
+    constructor() {
+        this.x = 0; // Starting position on X axis
+        this.y = 0 // Starting position on Y axis
+        this.direction = "right"; // Default Crash movement direction
+    }
+}
